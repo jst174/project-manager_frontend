b5c56d2c2c54f87b8d79a0628eae1ce98f464dfa
Eugene Stepanenko <jst1749.7@gmail.com>
2023-01-17 23:21:21 +0500

    fix bug with navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,24 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href=".">Project manager</Navbar.Brand>
+        <Navbar.Brand as={Link} to=".">
+          Project manager
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/employees">Employees</Nav.Link>
+            <Nav.Link as={Link} to="/employees">
+              Employees
+            </Nav.Link>
           </Nav>
           <Nav className="me-auto">
-            <Nav.Link href="/clients">Clients</Nav.Link>
+            <Nav.Link as={Link} to="/clients">
+              Clients
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
